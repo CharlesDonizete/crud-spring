d4496d27522fe0561db26ad10b37ed0eb45564bf
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-09-18 17:01:15 -0300

    45: Curso-Aulas: OneToMany

--- src/main/java/com/charles/crudspring/CrudSpringApplication.java
+++ src/main/java/com/charles/crudspring/CrudSpringApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 import com.charles.crudspring.enums.Category;
 import com.charles.crudspring.model.Course;
+import com.charles.crudspring.model.Lesson;
 import com.charles.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
@@ -20,15 +21,22 @@ public class CrudSpringApplication {
 	}
 
 	@Bean
-	CommandLineRunner initDatabase(CourseRepository courseRepository){
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
 		return args -> {
-			courseRepository.deleteAll();			
+			courseRepository.deleteAll();
 
 			List<Course> courses = new ArrayList<Course>();
 
 			Course c = new Course();
-			c.setName("Angular com Spring");
+			c.setName("Angular com Spring ");
 			c.setCategory(Category.FRONT_END);
+
+			Lesson l = new Lesson();
+			l.setName("Introdução");
+			l.setYoutubeUrl("watch?v=1");
+			l.setCourse(c);
+
+			c.getLessons().add(l);
 
 			courses.add(c);
 
@@ -36,11 +44,25 @@ public class CrudSpringApplication {
 			c1.setName("Spring");
 			c1.setCategory(Category.BACK_END);
 
+			Lesson l1 = new Lesson();
+			l1.setName("Introdução 2");
+			l1.setYoutubeUrl("watch?v=1");
+			l1.setCourse(c1);
+
+			c1.getLessons().add(l1);
+
 			courses.add(c1);
 
 			Course c2 = new Course();
 			c2.setName("ExtJS");
 			c2.setCategory(Category.FRONT_END);
+
+			Lesson l2 = new Lesson();
+			l2.setName("Introdução 3");
+			l2.setYoutubeUrl("watch?v=1");
+			l2.setCourse(c2);
+
+			c2.getLessons().add(l2);
 
 			courses.add(c2);
 
--- src/main/java/com/charles/crudspring/model/Course.java
+++ src/main/java/com/charles/crudspring/model/Course.java
@@ -1,5 +1,8 @@
 package com.charles.crudspring.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
@@ -10,12 +13,15 @@ import com.charles.crudspring.enums.converters.CategoryConverter;
 import com.charles.crudspring.enums.converters.StatusConverter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -23,11 +29,11 @@ import lombok.Data;
 
 @Data
 @Entity
-//@Table(name = "cursos")
+// @Table(name = "cursos")
 @SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
 @Where(clause = "status = 'Ativo'")
 public class Course {
-    
+
     @Id
     @JsonProperty("_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -39,14 +45,17 @@ public class Course {
     @Column(length = 100, nullable = false)
     private String name;
 
-    @NotNull    
+    @NotNull
     @Column(length = 10, nullable = false)
     @Convert(converter = CategoryConverter.class)
     private Category category;
 
-    @NotNull    
+    @NotNull
     @Column(length = 10, nullable = false)
     @Convert(converter = StatusConverter.class)
-    private Status status = Status.ACTIVE ;
+    private Status status = Status.ACTIVE;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "course")
+    private List<Lesson> lessons = new ArrayList<>();
 
 }
